19d224784228a8f572e599be80d3e26fdb5c982e
Eshashukla11 <eshashukla232001@gmail.com>
2024-07-22 00:31:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div className="section-1">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+function Navbar() {
+  return (
+    <div className="navbar-wrap">
+      <div className="img-wrap">
+        <img src="/brainSettings.png" alt="" />
+      </div>
+      <div className="nav-menu">
+        <p className="nav-menu-item">Services</p>
+        <GiHamburgerMenu className="hamburger-menu" />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
